ac2c28a9da6c4a542fb31f9f783d752e4b9c4f77
AlessiaCiabatti <alessiaciaba@gmail.com>
2024-03-19 15:50:42 +0100

    [add] js-numeroMaxEmail

--- js/script.js
+++ js/script.js
@@ -7,5 +7,21 @@ createApp({
       arrayEmailTotali: [],
       apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail'
     }
+  },
+  
+  methods:{
+    
+    // 1. ciclo for generare 10 email
+    numeroMaxEmail(){
+      for(let i = 0; i <= 10; i++){
+        if(this.arrayEmailTotali.length < 10){
+          this.arrayEmailTotali.push(data.response);
+        }
+      }
+      console.log(this.numeroMaxEmail)
+    }
+  },
+
+  mounted(){
   }
 }).mount('#app')
